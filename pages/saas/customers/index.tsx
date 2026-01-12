143820d63db7fcd58ffe96467bdf85b255a449cd
kalyansioxglobal <dev1@sioxglobal.com>
2026-01-12 18:32:32 +0530

    Fixes: UI fixes and pnl ui for hospitality

--- pages/saas/customers/index.tsx
+++ pages/saas/customers/index.tsx
@@ -88,7 +88,7 @@ export default function CustomersListPage() {
         {allowCreate && (
           <Link
             href="/saas/customers/new"
-            className="px-4 py-2 rounded-lg bg-blue-600 text-white text-sm font-medium hover:bg-blue-700"
+            className="btn"
           >
             + New Customer
           </Link>
